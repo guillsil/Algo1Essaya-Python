b539b412009ef92a9f3bb212bfb9e788028c0f0f
guillsil <111441275+guillsil@users.noreply.github.com>
2022-09-19 16:03:50 -0300

    new
    new

---
+++ parcialitos/primer_parcialito/33_ciclos_listas.py
@@ -0,0 +1,7 @@
+"""Escribir una función que reciba una lista y la invierta sobre la misma lista sin usar estructuras adicionales.
+Nota: Al usar slices (lista[::]) se crea una nueva lista, por lo que no se pueden utilizar para resolver este ejercicio"""
+def invertir_lista(lista):
+    for i in range(len(lista)//2):
+        lista[i], lista[-i-1] = lista[-i-1], lista[i]
+    return lista
+print(invertir_lista([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
